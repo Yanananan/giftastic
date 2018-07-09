8478e69b6cd78e23983bd55197aec813be5aff8e
Yanananan <meng.yanan@gmail.com>
2018-07-08 18:44:59 -0700

    added favorites to localStorage, adjusted styling

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,9 +1,37 @@
 var animals = ["owl","frog","beyonce","lemur"];
-var lastButton = "";
+var localData = JSON.parse(localStorage.getItem("allFavorites"));
+
+if (localData==null){
+    localStorage.setItem("allFavorites","{}");
+    localData = {};
+}
+
+var headerHeight;
+headerHeight = $("#header").height();
+$("#favDiv").css("margin-top",headerHeight+40+"px");
+console.log(headerHeight);
+
+for (var key in localData){
+    var favGif = localData[key];
+    $("#favDiv").append($("<div>").attr({"class":"gif","id":favGif.id}));
+    $("#"+favGif.id)
+    .append($("<div>").text("Title: "+favGif.title).addClass("gifText"))
+    .append($("<div>").text("Rating: "+favGif.rating).addClass("gifText"))
+    .append($("<img>").attr({"src":favGif.images.fixed_height_still.url,
+                             "data-still":favGif.images.fixed_height_still.url,
+                             "data-animate":favGif.images.fixed_height.url,
+                             "state":"still"}))
+    // .append($("<a>").html("&#x25BC").addClass("downloadButton").attr({"href":favGif.images.original.url,"download":key}));
+    //since I can't get the download button to work, i'm commenting out the download button
+    .append($("<button>").html("&#x2612").addClass("removeButton").attr({"data-id":favGif.id}));
+}
+
 //for loop to create buttons for animals in array above
-for (i in animals){
+for (var i in animals){
     $("#buttonDiv").append($("<button>").text(animals[i]).attr({"class":"animalButton"}));
 }
+
+
 
 //on click function to create buttons for new animals
 $(document).on("click","form button",function(){
@@ -11,21 +39,25 @@ $(document).on("click","form button",function(){
     var animal = $("input").val();
     $("#buttonDiv").append($("<button>").text(animal).attr({"class":"animalButton"}));
     $("input").val("");
+    headerHeight = $("#header").height();
+    $("#favDiv").css("margin-top",headerHeight+"px");
+    console.log(headerHeight);
 })
 
 //on click function to add gifs
 var animal = "";
 var apiKey = "zrFbMSNOTjrWkZbDs4haMGyhBG1xF7FG";
 var limit = 0;
-var gifStorage = [];
-var favGifID = 0;
+var tempStorage = [];//temp storage for everytime gifs are loaded onto the screen
+var lastButton = "";
+
 $(document).on("click",".animalButton",function(){
     animal = $(this).text();
         if (lastButton!=animal){
-            $("#gifDiv").empty();//gifs are emptied only if the last animal is different than this animal
+            $("#gifDiv").empty();//gifs are emptied only if the last animal is different than this animal, this way, each button press will add 10 more gifs to the bottom of the screen
             lastButton=animal;
             limit = 0;
-            gifStorage = [];
+            tempStorage = [];
         }
         limit = limit+10;
         var url = "http://api.giphy.com/v1/gifs/search?q="+animal+"&api_key="+apiKey+"&limit="+limit;
@@ -41,20 +73,16 @@ $(document).on("click",".animalButton",function(){
                                          "data-still":res.data[i].images.fixed_height_still.url,
                                          "data-animate":res.data[i].images.fixed_height.url,
                                          "state":"still"}))
-                .append($("<a>").html("&#x25BC").addClass("downloadButton").attr({
-                                                                                "href":res.data[i].images.original.url,
-                                                                                "download":res.data[i].id}))
+                // .append($("<a>").html("&#x25BC").addClass("downloadButton").attr({"gif-id":res.data[i].id}))
+                // .attr({"href":res.data[i].images.original.url,"download":res.data[i].id}))
+
                 //download button does not work, opens gif in new window instead
                 //<a href="path_to_file" download="proposed_file_name">Download</a>
-                .append($("<button>").html("&#x2605").addClass("favButton").attr({"data-id":i}));
-                gifStorage.push(res.data[i]);
+                .append($("<button>").html("&#x2665").addClass("favButton").attr({"data-id":i}));
+                tempStorage.push(res.data[i]);
             }
-            console.log(gifStorage);
+            console.log(tempStorage);
         });
-    
-    
-
-    
 })
 
 //on click function to start/stop gifs
@@ -80,18 +108,35 @@ $(document).on("click",".favButton",function(){
     // var url = "http://api.giphy.com/v1/gifs/search?id="+id+"&api_key="+apiKey;
     //https://media0.giphy.com/media/vQqeT3AYg8S5O/200.gif
     //https://media0.giphy.com/media/vQqeT3AYg8S5O/200_s.gif
-    var favGif = gifStorage[id];
-    //for favorites to persist after reload, the gifStorage array will probably have to move to another file or database
-    $("#favDiv").append($("<div>").attr({"class":"gif","id":"favGif"+favGifID}));
-    $("#favGif"+favGifID)
+    var favGif = tempStorage[id];
+    //for favorites to persist after reload, the tempStorage array will probably have to move to another file or database
+    $("#favDiv").append($("<div>").attr({"class":"gif","id":favGif.id}));
+    $("#"+favGif.id)
     .append($("<div>").text("Title: "+favGif.title).addClass("gifText"))
     .append($("<div>").text("Rating: "+favGif.rating).addClass("gifText"))
     .append($("<img>").attr({"src":favGif.images.fixed_height_still.url,
                              "data-still":favGif.images.fixed_height_still.url,
                              "data-animate":favGif.images.fixed_height.url,
                              "state":"still"}))
-    .append($("<a>").html("&#x25BC").addClass("downloadButton").attr({
-                                                                    "href":favGif.images.original.url,
-                                                                    "download":favGif.id}));
-    favGifID++;
+    // .append($("<a>").html("&#x25BC").addClass("downloadButton").attr({
+    //                                                                 "href":favGif.images.original.url,
+    //                                                                 "download":favGif.id}));
+    .append($("<button>").html("&#x2612").addClass("removeButton").attr({"data-id":favGif.id}));
+    //data-id is used to remove the data from localStorage
+    var dataID = favGif.id;
+    localData[dataID] = favGif;
+    localStorage.setItem("allFavorites",JSON.stringify(localData));
+})
+
+// $(document).on("click",".downloadButton",function(){
+//     window.open() = $(this).attr("gif-id")+".gif";
+// })              this doesn't work either...
+
+//on click function to remove favorited gif from localStorage and from screen
+$(document).on("click",".removeButton",function(){
+    var key = $(this).attr("data-id");
+    localData = JSON.parse(localStorage.getItem("allFavorites"));
+    delete localData[key];
+    localStorage.setItem("allFavorites",JSON.stringify(localData));
+    $(this).closest(".gif").remove();
 })
